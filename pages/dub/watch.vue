d636c3769f14093a540c7c71066cdb1422a6ea77
Ben Rucker <brucker117@gmail.com>
2021-12-19 19:04:58 -0500

    redirect old watch URLs to new

---
+++ pages/dub/watch.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    middleware: [
+        function({ redirect, route }) {
+            if (!route.query.s && !route.query.s)
+                redirect({
+                    name: "dub",
+                    query: { error: true },
+                });
+
+            let url = `/dub/watch/${route.query.s}/${route.query.e}`
+            if (route.query.t) query = `?t=${route.query.t}`
+            redirect(url);
+        }
+    ]
+}
+</script>
