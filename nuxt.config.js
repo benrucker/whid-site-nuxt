5251e8dc333a2158d990ec6c67c6f68d1f7470a2
Ben Rucker <brucker117@gmail.com>
2021-12-24 17:01:59 -0500

    Adjust nuxt config for echarts

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,17 +32,23 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    '@nuxt/typescript-build',
+    '@nuxtjs/composition-api/module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    // 'bootstrap-vue/nuxt',
     "@nuxt/content",
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [/echarts/, /zrender/]
   },
+
+  router: {
+    // trailingSlash: true,
+  }
 }
