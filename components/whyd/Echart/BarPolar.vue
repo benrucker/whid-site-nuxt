5aef6ddbdcbb69b2a18f11af741271b38426287b
Ben Rucker <brucker117@gmail.com>
2021-12-27 21:37:49 -0500

    Finish astrological sign

---
+++ components/whyd/Echart/BarPolar.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-chart class="chart" :option="option" ref="chart" />
+</template>
+
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { BarChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  ToolboxComponent,
+  GridComponent,
+  PolarComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+
+use([
+  CanvasRenderer,
+  BarChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  ToolboxComponent,
+  GridComponent,
+  PolarComponent,
+]);
+
+export default {
+  name: "HelloWorld",
+  props: {
+    data: [],
+    xAxis: [],
+    title: "",
+    bgColor: "",
+    color: "",
+    textColor: "",
+  },
+  components: {
+    VChart,
+  },
+  provide: {
+    [THEME_KEY]: "dark",
+  },
+  mounted() {
+    window.onresize = () => {
+      this.$refs.chart.resize();
+    };
+  },
+  data() {
+    return {
+      option: {},
+      lines: [],
+      labels: [],
+    };
+  },
+  methods: {
+    init(xAxis, data) {
+      this.option = {
+        polar: {
+          radius: [30, "80%"],
+        },
+        angleAxis: {
+          startAngle: 75,
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true,
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
+        },
+        radiusAxis: {
+          type: "category",
+          data: xAxis,
+          show: false,
+        },
+        series: [
+          {
+            name: this.title,
+            type: "bar",
+            data: data,
+            coordinateSystem: "polar",
+            roundCap: false,
+            itemStyle: {
+              borderRadius: [10, 10, 0, 0],
+            },
+          },
+        ],
+        color: [this.color],
+        backgroundColor: this.bgColor,
+        textStyle: {
+          color: this.textColor,
+          fontSize: 36,
+          fontWeight: "bold",
+        },
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chart {
+  min-height: 10px;
+}
+</style>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
--- components/whyd/diagnose/AstrologicalSign.vue
+++ components/whyd/diagnose/AstrologicalSign.vue
@@ -1,27 +1,46 @@
-<template></template>
+<template>
+  <div class="container d-flex flex-column w-50 pt-5">
+    <h4>you're {{ title }}</h4>
+    <p>better check your horoscope</p>
+    <div class="astroChart ratio ratio-4x3 w-75 align-self-center">
+      <WhydEchartBarPolar
+        :title="'Messages'"
+        :color="'#fff'"
+        :bgColor="'#C6CCFB'"
+        :textColor="'#303030'"
+        ref="astroChart"
+      />
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
-  props: {
-    urlPrefix: {
-      type: String,
-      default: "",
-    },
-    id: {
-      type: String,
-      default: "",
-    },
-  },
+  props: {},
   data() {
-    return {};
+    return {
+      title: "",
+      preposition: "",
+      timeRange: "",
+    };
   },
   async mounted() {},
   methods: {
-    async init() {
-      let data = await fetch(
-        `${this.urlPrefix}/user/${this.id}/monthCounts.json`
-      ).then((res) => res.json());
+    async init(id, urlPrefix) {
+      let data = await fetch(`${urlPrefix}/user/${id}/sign.json`).then(
+        (res) => res.json()
+      );
+      this.title = `${/^[AEIOU]/.test(data.maxSign) ? "an" : "a"} ${data.maxSign}`;
+      delete data.maxSign;
+
+      let signs = Object.keys(data);
+      let counts = Object.values(data);
+
+      this.$refs.astroChart.init(signs, counts);
     },
   },
 };
 </script>
+
+<style scoped>
+</style>
