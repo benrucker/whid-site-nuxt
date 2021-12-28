9741da83d533b0fa362abdb42ea5c58588818642
Ben Rucker <brucker117@gmail.com>
2021-12-28 03:28:11 -0500

    Add first word

---
+++ components/whyd/FirstMessage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h3>
+      to wrap things up, let's see the first thing that came out of your
+      mouth this year:
+    </h3>
+    <div class="pt-3 d-flex justify-content-center">
+      <WhydMessage :content="content" :attachments="attachments" class="w-50" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    urlPrefix: {
+      type: String,
+      default: "",
+    },
+    id: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      content: "",
+      attachments: [],
+    };
+  },
+  computed: {},
+  async mounted() {},
+  methods: {
+    async init(id, urlPrefix) {
+      let firstMsg = await fetch(
+        `${urlPrefix}/user/${id}/firstMessage.json`
+      ).then((res) => {
+        return res.json();
+      });
+      console.log(firstMsg);
+      this.content = firstMsg.content;
+      this.attachements = firstMsg.attachments;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.firstEverShadow {
+  box-shadow: 0px 0px 10px 3px #f3df4c;
+}
+
+div.firstEver::after {
+  content: "first in whole server!";
+}
+</style>
