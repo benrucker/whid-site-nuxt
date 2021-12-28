e7cdac2b46cbec528c0f9c9c142ff8873647265e
Ben Rucker <brucker117@gmail.com>
2021-12-28 03:28:33 -0500

    Add message component

---
+++ components/whyd/Message.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="message">
+    <div class="content">{{ content }}</div>
+    <div class="attachment" v-for="attachment in attachments" :key="attachment.id">
+      <img :src="attachment.url" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: "",
+    attachments: [],
+  },
+};
+</script>
+
+<style scoped>
+.message {
+    background: #222;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 10px rgba(0, 0, 0, 0.2);
+    padding: 1em;
+}
+
+img {
+    max-width: 100%;
+}
+</style>
