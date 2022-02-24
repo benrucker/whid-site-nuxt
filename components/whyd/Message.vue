d983e94e3eaaef31efa3627f7d2f26264c74eb86
Ben Rucker <brucker117@gmail.com>
2022-02-24 03:16:31 -0500

    Adjust style

--- components/whyd/Message.vue
+++ components/whyd/Message.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="message">
     <div class="content text-break">{{ content }}</div>
-    <div class="attachment" v-for="attachment in attachments" :key="attachment.id">
-      <video controls v-if="attachment.url.endsWith('.mp4')" >
-      <source :src="attachment.url" />
+    <div
+      class="attachment"
+      v-for="attachment in attachments"
+      :key="attachment.id"
+    >
+      <video controls v-if="attachment.url.endsWith('.mp4')">
+        <source :src="attachment.url" />
       </video>
       <img v-else :src="attachment.url" />
     </div>
@@ -14,20 +18,23 @@
 export default {
   props: {
     content: "",
-    attachments: [],
+    attachments: [{
+      id: "",
+      url: "",
+    }],
   },
 };
 </script>
 
 <style scoped>
 .message {
-    background: #222;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px 10px rgba(0, 0, 0, 0.2);
-    padding: 1em;
+  background: #222;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 10px rgba(0, 0, 0, 0.2);
+  padding: 1em;
 }
 
 img {
-    max-width: 100%;
+  max-width: 100%;
 }
 </style>
