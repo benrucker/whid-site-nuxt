5f873f0d1803e13c802c191cce61e38f5b8b435e
Ben Rucker <brucker117@gmail.com>
2022-03-28 02:49:56 -0400

    Add meta tags back

--- pages/dub/_season/_episode.vue
+++ pages/dub/_season/_episode.vue
@@ -20,153 +20,195 @@
       <h2 class="fw-bold mb-0 mt-1">
         {{ title }}
       </h2>
-      <p v-if="releaseDate" class="text-muted">
-        Released {{ releaseDate }}
-      </p>
+      <p v-if="releaseDate" class="text-muted">Released {{ releaseDate }}</p>
 
       <p v-for="(part, index) in parts" :key="index" class="mb-0">
-        <a
-          href="javascript:void(0)"
-          @click="goToPart(index)"
-        >Part {{ index + 1 }}</a>: {{ part.members }}
+        <a href="javascript:void(0)" @click="goToPart(index)"
+          >Part {{ index + 1 }}</a
+        >: {{ part.members }}
       </p>
-      <br>
+      <br />
     </div>
   </div>
 </template>
 <script>
 export default {
-  layout: 'dub-layout',
-  async asyncData ({ params, redirect }) {
-    const season = params.season
-    const episode = params.episode
+  layout: "dub-layout",
+  async asyncData({ params, redirect }) {
+    const season = params.season;
+    const episode = params.episode;
 
     return {
       season,
-      episode
-    }
+      episode,
+    };
   },
-  data () {
+  data() {
     return {
-      title: '',
-      releaseDate: '',
-      videoURL: '',
-      thumbnailURL: '',
+      title: "",
+      releaseDate: "",
+      videoURL: "",
+      thumbnailURL: "",
       parts: [],
       catalog: null,
       epData: null,
       time: 0,
-      loaded: false
-    }
+      loaded: false,
+    };
   },
-  async fetch () {
-    this.time = this.$nuxt.context.query.t
-    this.catalog = await this.$nuxt.$content('catalog').fetch()
+  async fetch() {
+    this.time = this.$nuxt.context.query.t;
+    this.catalog = await this.$nuxt.$content("catalog").fetch();
     try {
-      this.epData = getVideoDataFromID(this.catalog, this.season, this.episode)
+      this.epData = getVideoDataFromID(this.catalog, this.season, this.episode);
 
-      this.thumbnailURL = constructThumbnailURL(this.season, this.episode)
-      this.videoURL = constructVideoURL(this.season, this.episode)
-      this.title = this.epData.title
-      this.releaseDate = constructDate(this.epData)
-      this.parts = this.epData.parts
+      this.thumbnailURL = constructThumbnailURL(this.season, this.episode);
+      this.videoURL = constructVideoURL(this.season, this.episode);
+      this.title = this.epData.title;
+      this.releaseDate = constructDate(this.epData);
+      this.parts = this.epData.parts;
     } catch (err) {
-      if (err instanceof VideoIDError) { goToGallery() } else { throw err }
+      if (err instanceof VideoIDError) {
+        goToGallery();
+      } else {
+        throw err;
+      }
     }
-    this.loaded = true
+    this.loaded = true;
+  },
+  head() {
+    return {
+      title: "Watching " + this.title,
+      meta: [
+        ["theme-color", get_major_color()],
+        ["og:site_name", "whid.live"],
+        ["og:url", this.videoURL],
+        ["og:title", this.title],
+        ["og:image", this.thumbnailURL],
+        ["og:image:width", "1280"],
+        ["og:image:height", "720"],
+        ["og:description", "Watch what have i dubbed"],
+        ["og:type", "video.other"],
+        ["og:video:url", this.videoURL],
+        ["og:video:secure_url", this.videoURL],
+        ["og:video:type", "text/html"],
+        ["og:video:width", "1280"],
+        ["og:video:height", "720"],
+      ].map(([name, content]) => {
+        return {
+          hid: name,
+          property: name,
+          content: content,
+        };
+      }),
+    };
   },
   watch: {
-    async loaded () {
-      document.title = 'Watching ' + this.title
+    async loaded() {
+      document.title = "Watching " + this.title;
       const changeVol = () => {
         try {
-          this.$refs.video.volume = 0.4
-          this.goToTime(this.time)
-          return true
+          this.$refs.video.volume = 0.4;
+          this.goToTime(this.time);
+          return true;
         } catch {
-          return false
+          return false;
         }
-      }
-      const interval = () => setTimeout(() => {
-        if (changeVol()) {} else { setTimeout(interval, 50) }
-      }, 50)
-      interval()
-    }
+      };
+      const interval = () =>
+        setTimeout(() => {
+          if (changeVol()) {
+          } else {
+            setTimeout(interval, 50);
+          }
+        }, 50);
+      interval();
+    },
   },
-  async mounted () {
-    await this.setupPage()
+  async mounted() {
+    await this.setupPage();
   },
   methods: {
-    async setupPage () {},
-    goToPart (partIndex) {
-      this.goToTime(
-        convertTimestampToSeconds(this.parts[partIndex].timestamp)
-      )
+    async setupPage() {},
+    goToPart(partIndex) {
+      this.goToTime(convertTimestampToSeconds(this.parts[partIndex].timestamp));
     },
-    goToTime (time) {
-      this.$refs.video.currentTime = time
-    }
-  }
-}
+    goToTime(time) {
+      this.$refs.video.currentTime = time;
+    },
+  },
+};
 
 class VideoIDError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = 'VideoIDError'
+  constructor(message) {
+    super(message);
+    this.name = "VideoIDError";
   }
 }
 
-function getTimestampFromURL () {
-  const time = getParamFromURL('t')
-  if (time != null && time < 0) { throw new VideoIDError('Invalid timestamp') }
-  return time
+function getTimestampFromURL() {
+  const time = getParamFromURL("t");
+  if (time != null && time < 0) {
+    throw new VideoIDError("Invalid timestamp");
+  }
+  return time;
 }
 
-function getParamFromURL (key) {
-  const params = getParamsFromURL()
-  const param = params.get(key)
-  return param
+function getParamFromURL(key) {
+  const params = getParamsFromURL();
+  const param = params.get(key);
+  return param;
 }
 
-function getParamsFromURL () {
-  const uri = window.location.search.substring(1)
-  const params = new URLSearchParams(uri)
-  return params
+function getParamsFromURL() {
+  const uri = window.location.search.substring(1);
+  const params = new URLSearchParams(uri);
+  return params;
 }
 
-function getVideoDataFromID (catalog, season, id) {
-  const episodes = getEpisodesFromSeason(catalog, season)
-  const episode = getEpisodeFromList(episodes, id)
-  return episode
+function getVideoDataFromID(catalog, season, id) {
+  const episodes = getEpisodesFromSeason(catalog, season);
+  const episode = getEpisodeFromList(episodes, id);
+  return episode;
 }
 
-function getEpisodesFromSeason (catalog, season) {
-  return catalog.seasons[season].episodes
+function getEpisodesFromSeason(catalog, season) {
+  return catalog.seasons[season].episodes;
 }
 
-function getEpisodeFromList (episodes, epid) {
+function getEpisodeFromList(episodes, epid) {
   for (const episode of episodes) {
     if (episode.id === epid) {
-      return episode
+      return episode;
     }
   }
-  throw new VideoIDError('Video ID not found in catalog')
+  throw new VideoIDError("Video ID not found in catalog");
 }
 
-function constructVideoURL (season, episode) {
-  return '/dub/videos/' + season + '/' + episode + '.mp4'
+function constructVideoURL(season, episode) {
+  return "/dub/videos/" + season + "/" + episode + ".mp4";
 }
 
-function constructThumbnailURL (season, episode) {
-  return '/dub/thumbnails/' + season + '/' + episode + '.png'
+function constructThumbnailURL(season, episode) {
+  return "/dub/thumbnails/" + season + "/" + episode + ".png";
 }
 
-function constructDate (ep) {
-  return ep.releaseDate
+function constructDate(ep) {
+  return ep.releaseDate;
 }
 
-function convertTimestampToSeconds (timestamp) {
-  const [min, sec] = timestamp.split(':').map(x => Number(x))
-  return min * 60 + sec
+function convertTimestampToSeconds(timestamp) {
+  const [min, sec] = timestamp.split(":").map((x) => Number(x));
+  return min * 60 + sec;
+}
+
+function get_major_color() {
+  let cols = ["#ffd294", "#a3eff7", "#fcaecf", "#bda6ff"];
+  return cols[getRandomInt(cols.length)];
+}
+
+function getRandomInt(total) {
+  let newNum = Math.floor(Math.random() * (total - 1));
+  return newNum;
 }
 </script>
