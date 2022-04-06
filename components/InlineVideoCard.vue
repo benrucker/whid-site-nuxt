43fc159a5ed4beab0fda038db41e3285647e33ca
Ben Rucker <brucker117@gmail.com>
2022-04-05 20:32:01 -0400

    Add homepage

---
+++ components/InlineVideoCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <div class="ratio ratio-16x9">
+      <video
+        ref="video"
+        playsinline
+        controls
+        :src="videoLink"
+        :poster="thumbnailLink"
+        type="video/mp4"
+        class="mx-auto"
+        style="background-color: black"
+        preload="auto"
+      >
+        Sorry, your browser doesn't support embedded videos.
+      </video>
+    </div>
+    <NuxtLink :to="videoPlayerLink" class="text-reset text-decoration-none">
+      <div id="gallery-card" class="card-body">
+        <h5 class="card-title text-truncate pb-1 mb-0">
+          {{ title }}
+        </h5>
+        <h6 v-if="releaseDate" class="card-subtitle">
+          <small class="text-muted"> Released {{ releaseDate }}</small>
+        </h6>
+        <h6 v-if="description" class="card-subtitle mt-1">
+          <small class="text-muted">{{ description }}</small>
+        </h6>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    videoLink: String,
+    videoPlayerLink: String,
+    thumbnailLink: String,
+    title: String,
+    releaseDate: String,
+    description: String,
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,72 @@
-<template />
+<template>
+  <div>
+    <div class="container">
+      <div
+        class="
+          header
+          mt-5
+          d-flex
+          justify-content-center
+          fw-bold
+          align-items-center
+        "
+      >
+        <h1 class="fw-bolder px-2">Hi!</h1>
+        <img src="/miniwyatt.png" alt="hi!" class="px-2" />
+      </div>
 
-<script lang="ts">
-import Vue from 'vue'
+      <div class="welcome mt-5 mb-2 text-center">
+        Welcome to <span id="whid">what have i done</span>
+      </div>
 
-export default Vue.extend({})
+      <div class="description mt-5 text-center fs-5">
+        We are a group of friends who make stupid videos.
+      </div>
+      <div class="description mt-5 text-end fs-5">...like, really stupid.</div>
+    </div>
+
+    <div class="featured mt-5 container">
+      <InlineVideoCard
+        video-link="/dub/videos/extra/context2.mp4"
+        video-player-link="/dub/"
+        thumbnail-link="/dub/thumbnails/extra/context2.png"
+        title="what have i dubbed Out of Context"
+        release-date=""
+        description="Click here to see them all!"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
 </script>
+
+<style scoped>
+h1 {
+  font-size: 5rem;
+}
+
+.welcome {
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+#whid {
+  position: relative;
+  margin: 0.1em;
+}
+
+#whid:before {
+  content: "";
+  z-index: -1;
+  left: 0.1em;
+  top: 0.9em;
+  background-color: var(--bs-primary);
+  position: absolute;
+  width: 100%;
+  height: 25%;
+}
+
+.description {
+}
+</style>
