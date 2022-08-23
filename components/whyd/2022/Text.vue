e67177d149f1a170150f692166496a4a0bad88da
Ben Rucker <brucker117@gmail.com>
2022-08-22 20:05:36 -0700

    Init support for dynamic components

---
+++ components/whyd/2022/Text.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    {{ content }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
