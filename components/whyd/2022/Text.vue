0b0cc7bf79edab8ec5b2805279606afd99730ab9
Ben Rucker <brucker117@gmail.com>
2022-10-05 15:31:03 -0400

    Add style parsing

--- components/whyd/2022/Text.vue
+++ components/whyd/2022/Text.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-    {{ content }}
+    <span v-for="data in parsedContent" :key="data.text" :class="data.style">{{ data.text }}</span>
   </span>
 </template>
 
@@ -14,13 +14,43 @@ export default {
   },
   data () {
     return {
+      parsedContent: []
     }
   },
-  computed: {
-  },
   mounted () {
+    this.parsedContent = this.content.split(/(\{+.+?}+)/g).map(v => {
+      if (v.startsWith('{{') && v.endsWith('}}')) {
+        return parseValue(v)
+      } else if (v.startsWith('{') && v.endsWith('}')) {
+        return parseStyling(v)
+      } else {
+        return { text: v }
+      }
+    })
   },
-  methods: {
-  }
+}
+
+function parseStyling(data) {
+  const noCurlies = data.slice(1, -1)
+  const [text, styles] = noCurlies.split(' | ')
+  return { text, style: styles }
+}
+
+function parseValue(valueName) {
+  // TODO - parse value
+  const noCurlies = valueName.slice(2, -2)
+  return { text: `$${noCurlies}`, style: "number" }
 }
 </script>
+
+<style>
+.bold {
+  font-weight: bold;
+}
+.italic {
+  font-style: oblique;
+}
+.number {
+  font-family: monospace;
+}
+</style>
