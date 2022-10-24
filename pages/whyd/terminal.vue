12e8ccca04659968a985f76b8839257aa6e3557b
Ethan Fidler <ethancf1999@gmail.com>
2022-10-23 20:59:32 -0400

    add Terminal (#110)
    * initializing the terminal
    
    * terminal has buttons and you're pressing them
    
    Co-authored-by: Ben Rucker <brucker117@gmail.com>
    
    * breaking out components & init main terminal page
    
    Co-authored-by: Ben Rucker <brucker117@gmail.com>
    
    * real commands init
    
    * moving position of function
    
    Co-authored-by: Ben Rucker <brucker117@gmail.com>

---
+++ pages/whyd/terminal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    id="page"
+    @mouseup="terminalMouseUp"
+    @mousemove="terminalMouseMove($event)"
+  >
+    <Whyd2022Terminal ref="terminal" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  data() {
+    return {
+      terminalRef: null
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.terminalRef = this.$refs.terminal
+    })
+  },
+  methods: {
+    terminalMouseUp() {
+      this.$refs.terminal.terminalMouseUp()
+    },
+    terminalMouseMove(e) {
+      this.terminalRef.terminalMouseMove(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#page {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+</style>
