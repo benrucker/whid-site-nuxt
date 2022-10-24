b71e6eff368740af1bfbbf37d62a133208e43558
Ben Rucker <brucker117@gmail.com>
2022-10-23 17:24:52 -0700

    Hookup data and bump deps (#112)
    * Ignore data
    
    * Hookup data
    
    * Bump versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,21 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   extends: [
-    "@nuxtjs/eslint-config-typescript",
-    "plugin:nuxt/recommended",
-    "prettier",
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier'
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
-};
+  rules: {
+    'vue/first-attribute-linebreak': 'off',
+    'vue/no-child-content': 'off',
+    'vue/no-reserved-props': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'vue/valid-attribute-name': 'off',
+    'vue/valid-model-definition': 'off'
+  }
+}
