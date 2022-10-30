7fc2670b49f5ca2d4c94e1e3a74ad2105c1dc95f
Ben Rucker <brucker117@gmail.com>
2022-10-30 16:59:01 -0700

    Add more graph placeholders

---
+++ components/whyd/2022/Chat/Chart/MostUpvotedPost.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>most upvoted post</div>
+</template>
+
+<script>
+// This component will handle the animation and the swap to the fixed version
+
+export default {
+  props: {
+    stats: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {},
+  methods: {}
+}
+</script>
---
+++ components/whyd/2022/Chat/Chart/MostUsedReactions.vue
@@ -0,0 +1,27 @@
+<template>
+  <Whyd2022ChatChartMostUsedReactionsBroken v-if="broken" :stats="stats" />
+  <Whyd2022ChatChartMostUsedReactionsFixed v-else :stats="stats" />
+</template>
+
+<script>
+// This component will handle the animation and the swap to the fixed version
+
+export default {
+  props: {
+    stats: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      broken: true
+    }
+  },
+  methods: {
+    fix() {
+      this.broken = false
+    }
+  }
+}
+</script>
