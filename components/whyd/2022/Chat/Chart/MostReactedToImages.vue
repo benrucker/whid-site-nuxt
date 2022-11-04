fd6741825b4ba68f4c7a282973fdb99faf159e71
Ben Rucker <brucker117@gmail.com>
2022-11-03 20:00:24 -0700

    Add most used reactions graph (#148)
    * Add most reacted to framework
    
    * Add more graph placeholders
    
    * Init broken stuff
    
    * Add fixing animation

---
+++ components/whyd/2022/Chat/Chart/MostReactedToImages.vue
@@ -0,0 +1,27 @@
+<template>
+  <Whyd2022ChatChartMostReactedToImagesBroken v-if="broken" :stats="stats" />
+  <Whyd2022ChatChartMostReactedToImagesFixed v-else :stats="stats" />
+</template>
+
+<script>
+// This component will handle the animation and the swap to the fixed version
+
+export default {
+  props: {
+    stats: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      broken: true
+    }
+  },
+  methods: {
+    fix() {
+      this.broken = false
+    }
+  }
+}
+</script>
