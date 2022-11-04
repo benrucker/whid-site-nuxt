ab604abad74d409976a12c14c5f06a7ec5bcddda
Ben Rucker <brucker117@gmail.com>
2022-11-03 20:00:24 -0700

    Add most used reactions graph (#148)
    * Add most reacted to framework
    
    * Add more graph placeholders
    
    * Init broken stuff
    
    * Add fixing animation

---
+++ components/whyd/2022/Chat/Chart/MostDownvotedPost.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>most downvoted post</div>
+</template>
+
+<script>
+// This component will handle the animation and the swap to the fixed version
+
+export default {
+  props: {
+    stats: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {},
+  methods: {}
+}
+</script>
