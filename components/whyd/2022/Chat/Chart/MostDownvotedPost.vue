677343e7e72028af61f5b3bd201f1e93e54d595f
Ben Rucker <brucker117@gmail.com>
2022-11-05 14:44:42 -0700

    Add most downvoted and reacted-to posts (#157)
    * Add most downvoted post placeholder
    
    * Improve styling
    
    * Factor into message blocks
    
    * Add fixed most-reacted-images
    
    * Add broken reacted-to-images
    
    * Duplicated fix anim to images post

--- components/whyd/2022/Chat/Chart/MostDownvotedPost.vue
+++ components/whyd/2022/Chat/Chart/MostDownvotedPost.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>most downvoted post</div>
+  <Whyd2022ChatDiscordMessageBlock :messages="[message]" />
 </template>
 
 <script>
-// This component will handle the animation and the swap to the fixed version
-
 export default {
   props: {
     stats: {
@@ -12,7 +10,28 @@ export default {
       default: () => ({})
     }
   },
-  data() {},
+  data() {
+    return {
+      message: {
+        content: 'bad take or smn',
+        reactions: [
+          { name: 'downvote', count: 20 },
+          { name: 'cheeto', count: 5 },
+          { name: 'ahegao1', count: 1 },
+          { name: 'downvote2', count: 4 },
+          { name: 'downvote3', count: 5 }
+        ],
+        authorName: 'bebenebenebeb',
+        timestamp: '09/23/2022',
+        authorAvatar:
+          'https://cdn.discordapp.com/avatars/171471874969042945/052a661e1ec4f31166cc3e93dc588d4d.png?size=1024',
+        attachments: ['/miniwyatt.png', '/icon-l.png']
+      }
+    }
+  },
+  mounted() {},
   methods: {}
 }
 </script>
+
+<style scoped></style>
