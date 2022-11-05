d51eb304615940caa3c534978ca86bae235d96b8
Ben Rucker <brucker117@gmail.com>
2022-11-05 14:44:42 -0700

    Add most downvoted and reacted-to posts (#157)
    * Add most downvoted post placeholder
    
    * Improve styling
    
    * Factor into message blocks
    
    * Add fixed most-reacted-images
    
    * Add broken reacted-to-images
    
    * Duplicated fix anim to images post

--- components/whyd/2022/Chat/Chart/MostReactedToImages.vue
+++ components/whyd/2022/Chat/Chart/MostReactedToImages.vue
@@ -1,11 +1,25 @@
 <template>
-  <Whyd2022ChatChartMostReactedToImagesBroken v-if="broken" :stats="stats" />
-  <Whyd2022ChatChartMostReactedToImagesFixed v-else :stats="stats" />
+  <div>
+    <Whyd2022ChatChartMostReactedToImagesBroken v-if="broken" :stats="stats" />
+    <Whyd2022ChatChartMostReactedToImagesFixed v-else :stats="stats" />
+    <div v-if="fixing" class="hand fly-in">
+      <img src="/whyd/2022/hand1.png" />
+    </div>
+    <div v-if="fixed" class="hand bounce">
+      <img src="/whyd/2022/hand1.png" />
+    </div>
+    <div v-if="fixed" class="explosion">
+      <img src="/whyd/2022/explosion-fast.gif" />
+    </div>
+    <img
+      v-if="!fixing && !fixed"
+      style="display: none"
+      src="/whyd/2022/explosion-fast.gif"
+    />
+  </div>
 </template>
 
 <script>
-// This component will handle the animation and the swap to the fixed version
-
 export default {
   props: {
     stats: {
@@ -15,13 +29,115 @@ export default {
   },
   data() {
     return {
-      broken: true
+      broken: true,
+      fixing: false,
+      fixed: false
     }
   },
   methods: {
     fix() {
-      this.broken = false
+      setTimeout(() => {
+        this.$el.scrollIntoView({ behavior: 'smooth', block: 'center' })
+
+        setTimeout(() => {
+          // play slappy hand animation
+          this.fixing = true
+          setTimeout(() => {
+            this.fixing = false
+            this.fixed = true
+          }, 1000)
+
+          // play explosion gif
+
+          setTimeout(() => {
+            this.broken = false
+          }, 1000)
+        }, 500)
+      }, 2000)
     }
   }
 }
 </script>
+
+<style scoped>
+.hand {
+  position: absolute;
+  top: 40%;
+  left: 0;
+  transform: translate(0, 0);
+  transform-origin: right center;
+  width: 100px;
+  height: 100px;
+}
+
+.hand img {
+  width: 100%;
+  height: 100%;
+}
+
+.hand.fly-in {
+  animation: hand-fly-in 1s forwards cubic-bezier(0.6, -0.28, 0.735, 0.045);
+}
+
+.hand.bounce {
+  animation: hand-bounce 1s forwards cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  opacity: 1;
+}
+
+.explosion {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: fade-out-explosion 1s forwards step-end;
+}
+
+.explosion img {
+  width: 100%;
+  height: 100%;
+}
+
+@keyframes hand-fly-in {
+  0% {
+    transform: translate(-50vw, 0);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
+
+@keyframes hand-bounce {
+  0% {
+    transform: translate(0, 0);
+    opacity: 1;
+  }
+  25% {
+    rotate: 5deg;
+    opacity: 0.75;
+  }
+  50% {
+    rotate: -5deg;
+  }
+  75% {
+    rotate: 5deg;
+  }
+  100% {
+    rotate: -5deg;
+    transform: translate(-40px, -10px);
+    opacity: 0;
+  }
+}
+
+@keyframes fade-out-explosion {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ components/whyd/2022/Chat/DiscordMessage.vue
@@ -0,0 +1,316 @@
+<template>
+  <div class="message">
+    <div class="name-bar">
+      <img class="d-msg-avatar" :src="authorAvatar" />
+      <h2 class="name-ts">
+        <span class="name">{{ authorName }}</span>
+        <span class="timestamp">{{ timestamp }}</span>
+      </h2>
+    </div>
+    <div class="message-content">
+      {{ content }}
+    </div>
+    <div class="message-extras">
+      <div class="attachments">
+        <img
+          v-for="attachment in attachments"
+          :key="attachment"
+          :src="attachment"
+          class="attachment"
+        />
+      </div>
+      <div class="reactions">
+        <div
+          v-for="reaction in reactions"
+          :key="reaction.name"
+          class="reaction-bubble"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          data-bs-html="true"
+          data-bs-delay="500"
+          :title="`${count} people reacted with ${reaction.name}`"
+        >
+          <img
+            :src="`/whyd/2022/emojis/${reaction.name}.png`"
+            class="reaction-emoji"
+          />
+          <div class="reaction-count">{{ reaction.count }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      default: ''
+    },
+    reactions: {
+      type: Object,
+      default: () => ({})
+    },
+    authorName: {
+      type: String,
+      default: ''
+    },
+    authorAvatar: {
+      type: String,
+      default: ''
+    },
+    attachments: {
+      type: Array,
+      default: () => []
+    },
+    timestamp: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  min-height: 2.75rem;
+
+  padding: 2px 48px 2px 72px;
+
+  margin-top: 10px;
+  margin-bottom: 1em;
+
+  position: relative;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.message:hover {
+  background-color: rgba(4, 4, 5, 0.07);
+}
+
+.message.nohover:hover {
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.name-bar {
+  font-size: 0.9em;
+}
+
+.name-ts {
+  padding: 0;
+  margin: 0px !important;
+
+  font-weight: inherit;
+  font-style: inherit;
+  font-family: inherit;
+  font-size: 100%;
+  vertical-align: baseline;
+
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+}
+
+.name {
+  margin-right: 0.25em;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-weight: inherit;
+  font-style: inherit;
+  font-family: inherit;
+  font-size: 100%;
+
+  position: static;
+  margin-left: 0;
+  padding-left: 0;
+  text-indent: 0;
+}
+
+.timestamp {
+  position: relative;
+  min-height: 1.375em;
+
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-style: inherit;
+  font-family: inherit;
+
+  height: 1.25rem;
+  cursor: default;
+  pointer-events: none;
+  font-weight: 500;
+
+  margin-left: 0.25em;
+
+  font-size: 0.75em;
+  line-height: 1.375em;
+  color: #72767d;
+  vertical-align: baseline;
+}
+
+.d-msg-avatar {
+  position: absolute;
+  left: 18px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-top: 2px;
+  filter: drop-shadow(0px 0px 0px rgba(0, 0, 0, 0));
+  transition: filter 0.05s ease;
+}
+
+.d-msg-avatar:hover {
+  filter: drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.16));
+}
+
+.message-block a:link {
+  text-decoration: none;
+}
+
+.message-block a:hover .name {
+  text-decoration: underline;
+}
+
+.message-content {
+  font-size: 1em;
+  min-height: 4px;
+  color: #dcddde;
+}
+
+.message-extras {
+  display: grid;
+  grid-auto-flow: row;
+  grid-row-gap: 0.25rem;
+  text-indent: 0;
+  min-height: 0;
+  min-width: 0;
+  padding-top: 0.125rem;
+  padding-bottom: 0.125rem;
+  position: relative;
+}
+
+.attachments {
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+}
+
+.attachment {
+  justify-self: start;
+  -ms-flex-item-align: start;
+  align-self: start;
+  position: relative;
+  max-width: 100%;
+}
+
+.attachment img {
+  width: 90%;
+  border-radius: 4px;
+}
+
+@media (max-width: 576px) {
+  .attachment video {
+    width: 90%;
+    border-radius: 4px;
+    height: calc(100vw * 0.4);
+  }
+}
+@media (min-width: 576px) {
+  .attachment video {
+    width: 381px;
+    border-radius: 4px;
+    height: 214px;
+  }
+}
+@media (min-width: 768px) {
+  .attachment video {
+    height: calc(219px * 0.5625);
+    width: 219px;
+    border-radius: 4px;
+  }
+}
+@media (min-width: 992px) {
+  .attachment video {
+    height: calc(327px * 0.5625);
+    width: 327px;
+    border-radius: 4px;
+    /* height: 250px; */
+  }
+}
+@media (min-width: 1200px) {
+  .attachment video {
+    height: calc(408px * 0.5625);
+    width: 408px;
+    border-radius: 4px;
+    /* height: 250px; */
+  }
+}
+@media (min-width: 1400px) {
+  .attachment video {
+    height: calc(490px * 0.5625);
+    width: 490px;
+    border-radius: 4px;
+    /* height: 250px; */
+  }
+}
+
+.reactions {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+
+.reaction-bubble {
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: hsl(223, 6.9%, 19.8%);
+  border-radius: 7px;
+  border: 1px solid;
+  border-color: transparent;
+  /* width: 3.125em; */
+  padding: 0.075rem 0.375rem;
+  gap: 0.375rem;
+  margin-left: 0.25em;
+  margin-bottom: 0.25em;
+  cursor: pointer;
+
+  color: hsl(216, 3.7%, 73.5%);
+  font-weight: 600;
+  text-align: center;
+  transition: none 0.1s ease;
+  transition-property: background-color, border-color;
+}
+
+.reaction-bubble:hover {
+  color: #dcddde;
+  border-color: rgba(255, 255, 255, 0.2);
+  background-color: hsl(220, 7.7%, 22.9%);
+}
+
+.reaction-emoji {
+  max-width: 1em;
+  max-height: 1em;
+}
+
+.reaction-count {
+  font-size: 0.875em;
+  color: inherit;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  min-width: 9px;
+}
+
+.reaction-count:hover {
+  color: hsl(210, 2.9%, 86.7%);
+}
+</style>
