4cb32d3fe42a48cb217e8a7f76bce8409b1049e8
Ben Rucker <brucker117@gmail.com>
2022-11-05 13:53:02 -0700

    Add most downvoted post placeholder

--- components/whyd/2022/Chat/Chart/MostDownvotedPost.vue
+++ components/whyd/2022/Chat/Chart/MostDownvotedPost.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>most downvoted post</div>
+  <Whyd2022ChatDiscordMessage
+    :content="content"
+    :reactions="reactions"
+    :author-name="authorName"
+    :author-avatar="authorAvatar"
+    :attachments="attachments"
+    :timestamp="timestamp"
+  />
 </template>
 
 <script>
-// This component will handle the animation and the swap to the fixed version
-
 export default {
   props: {
     stats: {
@@ -12,7 +17,35 @@ export default {
       default: () => ({})
     }
   },
-  data() {},
+  data() {
+    return {
+      content: '',
+      reactions: {},
+      authorName: '',
+      authorAvatar: '',
+      attachments: [],
+      timestamp: ''
+    }
+  },
+  mounted() {
+    if (this.stats.server.mostDownvotedPost == null) {
+      this.content = 'Bad take'
+      this.reactions = {
+        downvote: 20,
+        cheeto: 5,
+        ahegao1: 1,
+        downvote2: 4,
+        downvote3: 5
+      }
+      this.authorName = 'bebenebenebeb'
+      this.authorAvatar =
+        'https://cdn.discordapp.com/avatars/171471874969042945/052a661e1ec4f31166cc3e93dc588d4d.png?size=1024'
+      this.attachments = ['/miniwyatt.png', '/whyd/camera.png']
+      this.timestamp = '09/25/2022'
+    }
+  },
   methods: {}
 }
 </script>
+
+<style scoped></style>
