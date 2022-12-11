9f4b8163d8f80a7b430eeddede64fdda05b23071
Ben Rucker <brucker117@gmail.com>
2022-12-11 14:12:10 -0800

    Add initial animation for terminal (#220)

--- pages/whyd/terminal.vue
+++ pages/whyd/terminal.vue
@@ -6,6 +6,7 @@
   >
     <img id="StickyNote" src="../../static/whyd/2022/sticky.png" />
     <Whyd2022Terminal id="Terminal" ref="terminal" />
+    <Whyd2022TerminalInitialAnimation v-if="shouldShowIntroAnimation" />
   </div>
 </template>
 
@@ -14,6 +15,7 @@ export default {
   layout: 'blank',
   data() {
     return {
+      shouldShowIntroAnimation: false,
       terminalRef: null
     }
   },
