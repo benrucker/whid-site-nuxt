a945fbe23e303fe38b9ef9805b46b23fd5af3fb3
Ben Rucker <brucker117@gmail.com>
2022-12-21 13:42:02 -0800

    minor changes (#283)

--- components/whyd/2022/Chat/Chart/MostReactedToImages/Fixed.vue
+++ components/whyd/2022/Chat/Chart/MostReactedToImages/Fixed.vue
@@ -16,7 +16,12 @@ export default {
   computed: {
     messages() {
       const threeMostReactedImages = this.stats.server['Most Reacted Image']
-      const ids = Object.keys(threeMostReactedImages).slice(0, 2)
+      const ids = Object.keys(threeMostReactedImages)
+        .filter((messageId) => {
+          // filter out the most upvoted image
+          return messageId !== '978860667186593792'
+        })
+        .slice(0, 2)
       return [
         threeMostReactedImages[ids[0]],
         threeMostReactedImages[ids[1]]
