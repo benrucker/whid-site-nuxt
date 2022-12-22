61b38d58b605e1f4fd3effffe7329bb8fd6dbf58
Ben Rucker <brucker117@gmail.com>
2022-12-21 19:21:22 -0800

    Use brendan cdn

--- plugins/util.js
+++ plugins/util.js
@@ -1,22 +1,26 @@
 const jsonPath = 'catalog.json'
 let catalog
 
+console.log('hi, this is the static pathway through util.js')
+
 class VideoIDError extends Error {
-  constructor (message) {
+  constructor(message) {
     super(message)
     this.name = 'VideoIDError'
   }
 }
 
-async function initCatalog () {
+async function initCatalog() {
   await loadCatalog()
   for (const seasonID in catalog.seasons) {
     const season = catalog.seasons[seasonID]
-    season.episodes.apply(function (x) { x.season = seasonID })
+    season.episodes.apply(function (x) {
+      x.season = seasonID
+    })
   }
 }
 
-async function loadCatalog () {
+async function loadCatalog() {
   const request = async () => {
     const response = await fetch(jsonPath)
     catalog = await response.json()
@@ -24,35 +28,37 @@ async function loadCatalog () {
   await request()
 }
 
-function getSeasons () { // called outside of file
+function getSeasons() {
+  // called outside of file
   return catalog.seasons
 }
 
-function getFeaturedVideo () {
+function getFeaturedVideo() {
   const [season, id, desc] = getFeaturedVideoData()
   const data = getVideoDataFromID(season, id)
   return [data, desc]
 }
 
-function getFeaturedVideoData () {
-  return [catalog.featured.season,
+function getFeaturedVideoData() {
+  return [
+    catalog.featured.season,
     catalog.featured.id,
     catalog.featured.description
   ]
 }
 
-function getVideoDataFromID (season, id) {
+function getVideoDataFromID(season, id) {
   const episodes = getEpisodesFromSeason(season)
   const episode = getEpisodeFromList(episodes, id)
   console.log(episode)
   return episode
 }
 
-function getEpisodesFromSeason (season) {
+function getEpisodesFromSeason(season) {
   return catalog.seasons[season].episodes
 }
 
-function getEpisodeFromList (episodes, epid) {
+function getEpisodeFromList(episodes, epid) {
   for (const episode of episodes) {
     if (episode.id === epid) {
       return episode
@@ -61,31 +67,31 @@ function getEpisodeFromList (episodes, epid) {
   throw new VideoIDError('Video ID not found in catalog')
 }
 
-function constructWatchURL (ep) {
+function constructWatchURL(ep) {
   return 'watch.php?s=' + ep.season + '&e=' + ep.id
 }
 
-function constructVideoURL (ep) {
+function constructVideoURL(ep) {
   return 'resources/videos/' + ep.season + '/' + ep.id + '.mp4'
 }
 
-function constructThumbnailURL (ep) {
+function constructThumbnailURL(ep) {
   return 'resources/thumbnails/' + ep.season + '/' + ep.id + '.png'
 }
 
-function constructDate (ep) {
+function constructDate(ep) {
   return ep.releaseDate
 }
 
-function goToGallery () {
+function goToGallery() {
   window.location.replace('../dub/?error=true')
 }
 
-function getErrorFromURL () {
+function getErrorFromURL() {
   return Boolean(getParamFromURL('error'))
 }
 
-function getSeasonAndEpisodeFromURL () {
+function getSeasonAndEpisodeFromURL() {
   const season = getParamFromURL('s')
   const ep = getParamFromURL('e')
   if (season === null || ep === null) {
@@ -94,22 +100,39 @@ function getSeasonAndEpisodeFromURL () {
   return [season, ep]
 }
 
-function getTimestampFromURL () {
+function getTimestampFromURL() {
   const time = getParamFromURL('t')
-  if (time != null && time < 0) { throw new VideoIDError('Invalid timestamp') }
+  if (time != null && time < 0) {
+    throw new VideoIDError('Invalid timestamp')
+  }
   return time
 }
 
-function getParamFromURL (key) {
+function getParamFromURL(key) {
   const params = getParamsFromURL()
   const param = params.get(key)
   return param
 }
 
-function getParamsFromURL () {
+function getParamsFromURL() {
   const uri = window.location.search.substring(1)
   const params = new URLSearchParams(uri)
   return params
 }
 
-export default { initCatalog, getSeasons, getFeaturedVideo, getVideoDataFromID, constructWatchURL, constructVideoURL, constructThumbnailURL, constructDate, goToGallery, getErrorFromURL, getSeasonAndEpisodeFromURL, getTimestampFromURL, getParamFromURL, getParamsFromURL }
+export default {
+  initCatalog,
+  getSeasons,
+  getFeaturedVideo,
+  getVideoDataFromID,
+  constructWatchURL,
+  constructVideoURL,
+  constructThumbnailURL,
+  constructDate,
+  goToGallery,
+  getErrorFromURL,
+  getSeasonAndEpisodeFromURL,
+  getTimestampFromURL,
+  getParamFromURL,
+  getParamsFromURL
+}
