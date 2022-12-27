fe1d7b75fdb6716433a9076fd8a01f8df95ae0b9
Ben Rucker <brucker117@gmail.com>
2022-12-27 13:59:54 -0800

    Add glitchiness to username page

--- pages/whyd/index.vue
+++ pages/whyd/index.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <div v-if="server" class="container text-center pt-5">
-      <img src="/whyd/2021/you.png" class="w-25 pb-4" />
-      <h1 class="pb-3">
-        <b>
-          <i class="title-text">what have you done 2022</i>
-        </b>
-      </h1>
+      <Whyd2022UsernameGlitchyHeader />
       <label for="nameInput" class="form-label" />
       <div class="input-group input-group-lg">
         <input
@@ -106,28 +101,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="scss">
-.title-text {
-  position: relative;
-  color: rgba(0, 0, 0, 0);
-  background: linear-gradient(99deg, #a3eff7, #ffd294, #fcaecf, #bda6ff);
-  overflow: clip;
-  background-clip: text;
-  background-size: 200% 100%;
-  background-position: 100%;
-  transition: background-position 0.5s ease;
-  text-decoration: none;
-
-  animation: background-move 10s ease-in-out infinite;
-}
-
-@keyframes background-move {
-  0% {
-    background-position: 100%;
-  }
-  50% {
-    background-position: 0%;
-  }
-}
-</style>
