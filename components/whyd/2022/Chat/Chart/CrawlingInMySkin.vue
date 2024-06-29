9ffc5de870e15752360dc44a412423e35159b854
Ben Rucker <brucker117@gmail.com>
2024-06-29 16:54:26 -0700

    Delete whyd components

--- components/whyd/2022/Chat/Chart/CrawlingInMySkin.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Whyd2022ChatChartMusic
-    header="Crawling"
-    subtitle="Linkin Park"
-    bg-img="/whyd/2022/music/crawling.jpg"
-    song="/whyd/2022/music/crawling.mp3"
-  />
-</template>
