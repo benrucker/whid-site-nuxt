e3bddc5e852fb87aacd0f14e4587c66e1ba23e24
Ben Rucker <brucker117@gmail.com>
2024-06-29 18:02:18 -0700

    Improve copy

--- pages/whyd.vue
+++ pages/whyd.vue
@@ -7,8 +7,8 @@
           navigation-url="https://whyd22.whid.live"
           thumbnail-link="/whyd/2022/gitcommit.png"
           title="whyd22"
-          release-date="sometime in 2023"
-          description="This is description text about whyd22"
+          release-date="1/1/2023"
+          description='The most recent installment of "what have you done." This rendition included a massive increase in scope, roping in 2 other devs and including a plotline, cameo appearances, and a second act.'
           :thumbnail-grid-size="5"
         />
         <FeaturedContentCard
@@ -16,8 +16,8 @@
           navigation-url="https://whyd21.whid.live"
           thumbnail-link="/whyd/2021/construction.png"
           title="whyd21"
-          release-date="sometime in 2022"
-          description="This is description text about whyd21"
+          release-date="12/30/2021"
+          description="The first entry in the yearly series, this experience was built in a rush at the end of the year. It was shown off at the infamous 2021 LAN party."
           :thumbnail-grid-size="5"
         />
         <FeaturedContentCard
@@ -25,8 +25,8 @@
           navigation-url="/whbd"
           thumbnail-link="/dub/images/ethan.png"
           title="5-year Anniversary Lookback"
-          release-date="sometime in 2021"
-          description="This is description text about the anniversary lookback"
+          release-date="4/26/2021"
+          description="To celebrate 5 years of chatting on Discord, this website briefly documented the history of the server, some overall statics, and included a small personalized section for each user at the end."
           :thumbnail-grid-size="5"
         />
       </div>
